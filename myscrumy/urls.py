63b6c03bb060a3679c038bb7085e5c23fb445875
cugbo <64258165+cugbo@users.noreply.github.com>
2020-05-23 08:11:09 +0000

    Update urls.py

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import path, include
 
-from chimaugbscrumy import views
+from chimaugboscrumy import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
