2f22d8a890750584f1a4c87fc787dc36eec402c7
cugbo <64258165+cugbo@users.noreply.github.com>
2020-05-23 07:55:27 +0000

    Update urls.py

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import path, include
+from django.conf.urls import url, include
 
 from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chimaugboscrumy/', include('chimaugboscrumy.urls')),
-    path('admin/', admin.site.urls),
-    path('django-chimaugboscrumy/', include('django-chimaugboscrumy.urls'))
+    url('admin/', admin.site.urls),
+    url('chimaugboscrumy/', include('chimaugboscrumy.urls')),
+    url('admin/', admin.site.urls),
+    url('django-chimaugboscrumy/', include('django-chimaugboscrumy.urls'))
 ]
