c828edefcc4e9da29450bd582e64bc3247ad69bb
cugbo <64258165+cugbo@users.noreply.github.com>
2020-05-23 07:05:32 +0000

    Update urls.py

--- chimaugboscrumy/urls.py
+++ chimaugboscrumy/urls.py
@@ -1,10 +1,8 @@
-#Django
+# Django
 from django.urls import path
 
-#local
 from . import views
 
 urlpatterns = [
-    path('', views.get_grading_parameters, name='get_grading_parameters'),
+    path('', views.index, name='index'),
 ]
-
