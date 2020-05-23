b0f16575f65c5d4d54e1b9da301eb416ee17229f
cugbo <64258165+cugbo@users.noreply.github.com>
2020-05-23 07:10:05 +0000

    Update settings.py

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chimaugboscrumy',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,9 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'ugbo',
+        'USER': 'cugbo',
+        'PASSWORD': 'ch34m1',
     }
 }
 
