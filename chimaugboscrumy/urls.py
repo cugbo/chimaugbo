06a3344b4b2af62711851788b7c8e6f97c9589af
cugbo <64258165+cugbo@users.noreply.github.com>
2020-05-23 07:53:26 +0000

    Update urls.py

--- chimaugboscrumy/urls.py
+++ chimaugboscrumy/urls.py
@@ -1,8 +1,8 @@
 # Django
-from django.urls import path
+from django.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url('', views.index, name='index'),
 ]
