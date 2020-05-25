d9a675200e7edaaec934382d5e2ceb9e035abaaf
cugbo <64258165+cugbo@users.noreply.github.com>
2020-05-25 13:57:32 +0100

    Update admin.py

--- chimaugboscrumy/admin.py
+++ chimaugboscrumy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from chimaugboscrumy.models import GoalStatus, ScrumyGoals, ScrumyHistory
+from . models import GoalStatus, ScrumyGoals, ScrumyHistory
 
 # Register your models here.
 admin.site.register(GoalStatus)
